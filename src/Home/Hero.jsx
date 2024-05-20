d91e3e647b565b32285d9a7ba69b52d474826f1b
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-05-20 16:54:59 +0200

    adjust

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -12,8 +12,8 @@ const Hero = () => {
                 <img src={image.ScrollDown} alt="" width="100px" className=" absolute right-0 top-7" />
             </div>
 
-            <div className=" max-xl: h-full">
-                <h1 className='text-center font-bold leading-snug  text-[2.2rem] md:text-8xl md:h-full flex flex-col md:justify-center items-center pt-40 md:pt-0'>Captivating <span className="text3d whitespace-nowrap">3D Models</span> & <span className="beating-text block w-full">Product Enhancement</span></h1>
+            <div className="flex flex-col justify-center items-center h-[80%]">
+                <h1 className='text-center font-bold leading-snug  text-[2.5rem] md:text-8xl flex flex-col md:justify-center items-center '>Captivating <span className="text3d whitespace-nowrap">3D Models</span> & <span className="beating-text block w-full">Product Enhancement</span></h1>
             </div>
         </main>
     )
